140ccfca6c3bb1ea70f5a079e3a4eabd36e36567
akoufos <akoufos@4280468b-92cb-4420-8b2c-ef5392c4ed84>
2014-03-17 18:04:39 +0000

    Updated global module to include number of different types of atoms, number of atoms per atom type, and updated how the parameters of number of secular equations and number of self-energies are defined. Also added commentation to the global module file.

--- global.f90
+++ global.f90
@@ -1,9 +1,20 @@
 module global
+! Currently this global file is setup for calculations of P4/nmm Fe2Se/Te2
+!-------------------------------------------------------------------------
+! jsz - Number of kpoints
+! ntype - Number of different atom types
+! natom(ntype) - Number of different atoms (# of atoms in should 
+  ! be given alphabetical order; i.e. UPd2Al3 should be (3,2,1))
+! nse - Number of self energies
+! sec - Number of secular equations
+! verbose - Used for debug information to screen
+!-------------------------------------------------------------------------
 implicit none
-! jsz = # of kpoints
-! nse = # of self energies [2*states (2 for real and imag)]
-! sec = # of secular equations
-integer(4), parameter :: jsz = 904, nse = 8, sec = 36
+integer(4), parameter :: jsz = 904 ! From TB band calculation
+integer(4), parameter :: ntype = 2 ! Fe & Se
+integer(4), parameter :: natom(ntype) = (/ 2, 2 /) ! Fe = 2 , Se/Te = 2
+integer(4), parameter :: nse = 4*natom(2) ! s(1) + p(3) for each Se/Te
+integer(4), parameter :: sec = 9*sum(natom) ! 9 (s(1) + p(3) + d(5))
 logical :: verbose
 real(8), parameter :: pi = 4.0d0*datan(1.0d0), pp = 1.0d0/pi
 end module global
