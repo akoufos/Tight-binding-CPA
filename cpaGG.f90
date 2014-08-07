806163c18dde82c304104a1efd230df869aeb35a
akoufos <akoufos@4280468b-92cb-4420-8b2c-ef5392c4ed84>
2014-08-07 15:00:03 +0000

    Minor update. cpaGG updated to calculated the individual etas and lambdas correctly, as well as produce better outputting information.

--- cpaGG.f90
+++ cpaGG.f90
@@ -20,7 +20,7 @@ subroutine cpaGG(mode, fermi, nelec, nef, N)
 !   1 - Calculate during CPA program
 !   2 - Calculate at a later date of CPA completion
 ! mt1/2 - Muffin-tin ratio used to correct for lack of muffin-tins in the
-!   TB-CPA formulism used
+!   TB-CPA formulism used (Version 3+: Not used for GG calculations.)
 ! N(12) - Angular momentum components of the DOS (Ry)
 ! nef - Total DOS at the Fermi level (Ry)
 ! nelec - Number of electrons
@@ -46,6 +46,16 @@ subroutine cpaGG(mode, fermi, nelec, nef, N)
 ! V2 - 06-09-2014 - Added more information for the input file, allowing for
 ! better approximations of the effective quanities used as final inputs for
 ! the Gaspari-Gyorffy method.
+!
+! V3 - 06-17-2014 - Changed the equations for calculating etas. Now have
+! etas calculated for sp and pd components. The constant used, now come
+! from the sp, pd, df components of eta calculated with LAPW results. This
+! new constant is calculated by the equation 
+!          eta = C*sum[N_l*N_{l+1}/N_t]
+! where N_l is the decomposed DOS and N_t is the total DOS, all at
+! the Fermi level. C is determined for l=1,3, where the CPA equation for
+! eta only uses l=1,2 (assumes f-DOS is zero). CPA equation is given as
+!          eta = C(N_s*N_p/N_t + N_p*N_d/N_t)
 !--------------------------------------------------------------------------
 use global
 use unitconvert
@@ -53,9 +63,9 @@ use concentration
 implicit none
 integer(kind=4) :: i
 integer(kind=4), intent(in) :: mode
-real(kind=8) :: eta(ntype), lambda(ntype), Tc, M(3), w2(ntype), effw2, &
-  effM, effM2, effl, ratio, sfratio, w, lsf, must, C(ntype), effTheta, &
-  Theta(ntype), Theta2, mt, dos(12)
+real(kind=8) :: eta(3), lambda(3), Tc, M(3), w2(ntype), effw2, &
+  effM, effM2, effl, ratio, sfratio, w, lsf, must, C(2*ntype), effTheta, &
+  Theta(ntype), Theta2, mt, dos(12), eta1(2), eta2(2), eta3(2), C1, C2, C3
 real(kind=8), intent(inout) :: fermi, nelec, nef, N(12)
 real(kind=8), parameter :: mt1 = 1.d0/26.920d0
 real(kind=8), parameter :: mt2 = 1.d0/36.4143d0
@@ -85,10 +95,10 @@ dos(3:6) = N(3:6)
 dos(7:) = N(7:)*mt
 write(11,'(A)')'LAPW like DOS'
 write(11,1101)
-write(11,1102)fermi,nelec,nef,(dos(i),i=1,12)
+write(11,1102)fermi,nelec,2.0d0*nef,(2.0d0*dos(i),i=1,12)
 open(10,file='superCPA.in')
 read(10,*) (atom(i),i=1,3)
-read(10,*) C(1), C(2)
+read(10,*) (C(i),i=1,4)
 read(10,*) (M(i),i=1,3)
 read(10,*) Theta(1), Theta(2)
 read(10,*) must
@@ -96,12 +106,32 @@ read(10,*) lsf
 close(10)
 effTheta = Theta(2) + con*(Theta(1)-Theta(2))
 Theta2 = effTheta**2.0d0/2.0d0
-! eta(1) contains the muffin-tin adjustment for the s and p states, but
-! leaves the d states untouched due to localization found in LAPW results
-eta(1) = C(1)*sum(dos(1:6))/nef
-eta(2) = C(2)*sum(dos(7:8))/nef
-write(11,1103)'Hopfield parameter of Fe (eV/angstrom^2):',eta(1)
-write(11,1103)'Hopfield parameter of Se/Te (eV/angstrom^2):',eta(2)
+! Calculate the etas using LAPW constants and CPA DOS. 
+!!!!!! No need for muffin-
+!!!!!! tin corrections.
+C1 = C(3) + con*(C(1)-C(3)) ! atom type 1 eta constant
+!C2 = C(4) + con*(C(2)-C(4)) ! atom type 2 eta constant
+C2 = C(2) ! atom type 2 eta constant
+C3 = C(4) ! atom type 3 eta constant
+eta1(1) = C1*(dos(1)*dos(2)/nef)
+eta1(2) = C1*(dos(2)*sum(dos(3:6))/nef)
+eta2(1) = C2*(dos(9)*dos(10)/nef)
+eta2(2) = C2*(dos(10)*0.1d0/nef)
+eta3(1) = C3*(dos(11)*dos(12)/nef)
+eta3(2) = C3*(dos(12)*0.1d0/nef)
+eta(1) = sum(eta1)
+eta(2) = sum(eta2)
+eta(3) = sum(eta3)
+write(6,1104)'Eta '//atom(1)//' sp and pd: ',eta1
+write(6,1104)'Eta '//atom(2)//' sp and pd: ',eta2
+write(6,1104)'Eta '//atom(3)//' sp and pd: ',eta3
+write(11,1104)'Eta '//atom(1)//' sp and pd: ',eta1
+write(11,1104)'Eta '//atom(2)//' sp and pd: ',eta2
+write(11,1104)'Eta '//atom(3)//' sp and pd: ',eta3
+write(11,*)
+write(11,1103)'Hopfield parameter of '//atom(1)//' (eV/angstrom^2):',eta(1)
+write(11,1103)'Hopfield parameter of '//atom(2)//' (eV/angstrom^2):',eta(2)
+write(11,1103)'Hopfield parameter of '//atom(3)//' (eV/angstrom^2):',eta(3)
 !write(11,1103)'Total eta (eV/angstrom^2):',sum(eta)
 ! Calculate the effective atomic mass of the system
 effM2 = M(3) + con*(M(2)-M(3))
@@ -113,6 +143,7 @@ write(11,1103)'Total Effective Atomic Mass (u):',effM
 write(11,*)
 write(11,1103)'Debye temperature of '//atom(1)//atom(2)//' (K^2):',Theta(1)
 write(11,1103)'Debye temperature of '//atom(1)//atom(3)//' (K^2):',Theta(2)
+write(11,1103)'Effective Debye temperature of system (K^2):',effTheta
 ! Calculate the average sqaured phonon frequencies and effective values
 w2(:) = Theta(:)**2.0d0/2.0d0*K2meV*K2meV
 effw2 = w2(2) + con*(w2(1)-w2(2))
@@ -129,7 +160,8 @@ write(11,*)
 !lambda(1) = eta(1)/(M(1)*effw2*u2kg*meV2Hz*meV2Hz*kg_ss2eV_AA)
 !lambda(2) = eta(2)/(effM2*effw2*u2kg*meV2Hz*meV2Hz*kg_ss2eV_AA)
 lambda(1) = eta(1)/(M(1)*Theta2*uKK2eV_AA)
-lambda(2) = eta(2)/(effM2*Theta2*uKK2eV_AA)
+lambda(2) = eta(2)/(M(2)*Theta2*uKK2eV_AA)
+lambda(3) = eta(3)/(M(3)*Theta2*uKK2eV_AA)
 effl = sum(lambda)
 print*,(u2kg*K2eV*K2eV*eV2Hz*eV2Hz*kg_ss2eV_AA)
 print*,(M(1)*effw2*u2kg*meV2Hz*meV2Hz*kg_ss2eV_AA)
@@ -140,8 +172,10 @@ print*,(M(1)*Theta(1)**2.0d0/2.0d0*uKK2eV_AA2)
 print*,(effM2*Theta(1)**2.0d0/2.0d0*uKK2eV_AA2)
 write(11,1103)'Electron-phonon coupling constant of '//atom(1)//':', &
   lambda(1)
-write(11,1103)'Electron-phonon coupling constant of '//atom(2)//'/'// &
-  atom(3)//':',lambda(2)
+write(11,1103)'Electron-phonon coupling constant of '//atom(2)//':', &
+  lambda(2)
+write(11,1103)'Electron-phonon coupling constant of '//atom(3)//':', &
+  lambda(3)
 write(11,1103)'Total electron-phonon coupling constant:',effl
 ! Calculate the critical superconducting temperature
 ratio = -1.04d0*(1.0d0+effl) / (effl-must*(1.0d0+0.62d0*effl))
@@ -166,5 +200,6 @@ return
   Se-s       Se-p       Te-s       Te-p')
 1102 format(15(F10.6,1X),/)
 1103 format(A55,1X,F10.6)
+1104 format(A20,(2(1X,F10.6)))
 2000 format('End cpaGG',/)
 end subroutine cpaGG
